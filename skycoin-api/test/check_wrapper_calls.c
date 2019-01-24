d99dffdad4744f2411af9f126b4ba91e8434d6b6
Alvaro Denis <dev0003@simelo.tech>
2019-01-24 13:06:01 -0500

    add assert field initialization for assert in xUnit tests struct ref #47

--- skycoin-api/test/check_wrapper_calls.c
+++ skycoin-api/test/check_wrapper_calls.c
@@ -2,14 +2,19 @@
 
 #include <stdbool.h>
 
-#include "check.h"
+#include <check.h>
 
 void assert_msg(bool condition, const char * format, ...) {
 	ck_assert_msg(condition, format);
 }
 
+void assert(bool condition) {
+	ck_assert(condition);
+}
+
 xUnitTest create_test_framework_wrapper() {
-	xUnitTest chech_test_wrapper;
-	chech_test_wrapper.assert_msg = assert_msg;
-	return chech_test_wrapper;
+	xUnitTest check_test_wrapper;
+	check_test_wrapper.assert_msg = assert_msg;
+	check_test_wrapper.assert = assert;
+	return check_test_wrapper;
 }
